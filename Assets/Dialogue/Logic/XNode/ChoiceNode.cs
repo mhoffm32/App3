a3b84e382f6b62c28a1f72746abcd490d8d6a283
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2024-11-25 11:42:50 -0500

    feat: Logic for dialogue and example dialogue tree

---
+++ Assets/Dialogue/Logic/XNode/ChoiceNode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using XNode;
+
+public class ChoiceNode : BaseNode {
+
+	[Input] public int entry;
+	public string speakerName;
+	public string dialogueLine;
+	public Sprite sprite;
+	[Output(dynamicPortList = true)] public string[] responses; // Responses to this dialogue
+
+
+	public override string GetString() {
+		return "ChoiceNode/" + speakerName + "/" + dialogueLine;
+	}
+
+	public override Sprite GetSprite() {
+		return sprite;
+	}
+}
