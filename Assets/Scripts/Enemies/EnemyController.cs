61052a49ab402bffa6ea29fcd1d55c77e1550d81
Madelyn Hoffman <mhoffm32@uwo.ca>
2024-11-29 22:40:46 -0500

    Zombie Attack

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    
+    public Player player;// Reference to the player
+    public Enemy[] enemies;  // The dialogue graph if the player has a specific item
+
+    private NodeParser nodeParser; // Reference to the NodeParser script
+
+    private void Start()
+    {
+        enemies = GetComponentsInChildren<Enemy>();
+        nodeParser = GetComponent<NodeParser>();
+        if (nodeParser == null)
+        {
+            Debug.LogError("NodeParser script is missing on this NPC!");
+        }
+    }
+    private void Update()
+    {
+        if (AreAllEnemiesDead())
+        {
+            player.allEnemiesKilled = true;
+            player.northQuestComplete = true;
+            // Add your logic for when all enemies are dead
+        }
+    }
+
+    private bool AreAllEnemiesDead()
+    {
+        // Check if all enemies' isDead property is true
+        return enemies != null && enemies.All(enemy => enemy.isDead);
+    }
+}
